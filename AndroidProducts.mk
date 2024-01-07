657bdb474b90b82797b58264d636774c650c264e
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2024-01-07 20:50:05 +0100

    RMX2030: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: arm64 Manufacturer: realme Model: RMX2030 Script version: 3.0.0

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_RMX2030.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_RMX2030-user \
+    omni_RMX2030-userdebug \
+    omni_RMX2030-eng
---
+++ README.md
@@ -0,0 +1,10 @@
+# Android device tree for realme RMX2030 (RMX2030)
+
+```
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+```
